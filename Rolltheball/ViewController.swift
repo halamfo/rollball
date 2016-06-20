c84295d5c3c099adfa3d5cc52e8bde470892aeaa
halamfo <tungnt991@gmail.com>
2016-06-20 20:16:11 +0700

    Initial Commit

---
+++ Rolltheball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rolltheball
+//
+//  Created by Dân Tơi on 6/20/16.
+//  Copyright © 2016 Dân Tơi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
